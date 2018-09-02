5cadb23a3972109432588cfa6c298daae876e57b
nilzmoradi94 <nilzmoradi94@gmail.com>
2018-09-02 11:11:10 +0430

    My project setup  Completed Successfully

---
+++ shoppingbackend/src/main/java/com/usc/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.usc.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
